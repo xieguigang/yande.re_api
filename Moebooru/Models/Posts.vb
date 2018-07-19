0748918d4a7276d3256c4c73af918b7d32aaa056
xieguigang <xie.guigang@gcmodeller.org>
2018-07-19 22:25:13 +0800

    add api framework

--- Moebooru/Models/Posts.vb
+++ Moebooru/Models/Posts.vb
@@ -1,3 +1,24 @@
-﻿Public Class Posts
+﻿Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.ApplicationServices.Debugging
+
+Public Class Posts
+
+    <XmlAttribute> Public Property count As Integer
+    <XmlAttribute> Public Property offset As Integer
+
+    <XmlElement("post")>
+    Public Property posts As post()
+
+End Class
+
+Public Class post : Implements IVisualStudioPreviews
+
+    Public ReadOnly Property Previews As String Implements IVisualStudioPreviews.Previews
+        Get
+            Return (<img src=<%= sample_url %> style="width:100%; height:100%;"/>).ToString
+        End Get
+    End Property
+
+    Public Property sample_url As String
 
 End Class
--- Moebooru/api.vb
+++ Moebooru/api.vb
@@ -1,3 +1,41 @@
-﻿<HideModuleName> Public Module api
+﻿Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.CommandLine.Reflection
 
+<HideModuleName> Public Module api
+
+    Const base$ = "https://yande.re"
+
+    ReadOnly apis As Dictionary(Of String, String)
+
+    Private Function getURL(<CallerMemberName> Optional key$ = Nothing) As String
+        Return $"{api.base}/{apis(key)}"
+    End Function
+
+    Sub New()
+        apis = GetType(api) _
+            .GetMethods(BindingFlags.Public) _
+            .Where(Function(m)
+                       Return Not m.GetCustomAttribute(Of ExportAPIAttribute) Is Nothing
+                   End Function) _
+            .ToDictionary(Function(m) m.Name,
+                          Function(m)
+                              Return m.GetCustomAttribute(Of ExportAPIAttribute).Name
+                          End Function)
+    End Sub
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="limit%">How many posts you want to retrieve. There is a hard limit of 100 posts per request.</param>
+    ''' <param name="page%">The page number.</param>
+    ''' <param name="tags">
+    ''' The tags to search for. Any tag combination that works on the web site will work here. This includes all the meta-tags.
+    ''' </param>
+    ''' <returns></returns>
+    <ExportAPI("post.xml")>
+    Public Function Posts(Optional limit% = -1, Optional page% = -1, Optional tags As IEnumerable(Of String) = Nothing) As Posts
+        Dim url$ = getURL()
+
+    End Function
 End Module
