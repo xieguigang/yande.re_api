b59ae93dbe7779d1e2a4f25f163973a563f71cf5
xieguigang <xie.guigang@gcmodeller.org>
2018-07-21 00:12:21 +0800

    config for test

--- test/Module1.vb
+++ test/Module1.vb
@@ -2,7 +2,9 @@
 
     Sub Main()
         'Dim p = Moebooru.Posts
-        Dim p = Moebooru.PoolShow(5022)
+        ' Dim p = Moebooru.PoolShow(5022)
+
+        Call Moebooru.DownloadPool(5022, "D:/tmp/5022")
 
         Pause()
     End Sub
