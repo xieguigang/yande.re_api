09c7c3029ba9160d2a3f644208b6ab649edc4e50
xieguigang <xie.guigang@gcmodeller.org>
2018-07-21 02:36:22 +0800

    renames folder

---
+++ fetch/Program.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.VisualBasic.Serialization.JSON
+
+Module Program
+
+    Sub Main()
+        Dim pool_id$ = App.CommandLine.Name
+        Dim EXPORT$ = App.CommandLine("/export") Or $"./{pool_id}/"
+
+        Call Moebooru.DownloadPool(pool_id, EXPORT).ToArray
+        Call Moebooru.CheckPoolIntegrity(EXPORT) _
+                     .GetJson _
+                     .PrintException
+    End Sub
+End Module
