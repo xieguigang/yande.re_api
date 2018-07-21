6d327cca59959ab18e3396881a477cac10df7111
xieguigang <xie.guigang@gcmodeller.org>
2018-07-21 21:54:35 +0800

    add task pending

--- fetch/Program.vb
+++ fetch/Program.vb
@@ -2,6 +2,8 @@
 
 Module Program
 
+    ReadOnly pendingTask$ = App.LocalData & "/pending.json"
+
     Sub Main()
         Dim pool_id$ = App.CommandLine.Name
         Dim EXPORT$ = App.CommandLine("/export") Or $"./{pool_id}/"
@@ -9,11 +11,39 @@ Module Program
         If pool_id.StringEmpty Then
             Call Console.WriteLine("Usage: fetch <pool_id> [/export <directory>]")
             Return
-        End If
+        ElseIf pool_id.TextEquals("pending") Then
+            pool_id = App.CommandLine.Parameters(0)
 
+            If Not pendingTask.FileExists Then
+                Call New Dictionary(Of String, String) From {
+                    {pool_id, 0}
+                }.GetJson _
+                 .SaveTo(pendingTask)
+            Else
+                With pendingTask.LoadObject(Of Dictionary(Of String, String))
+                    .Item(pool_id) = 0
+                    .GetJson _
+                    .SaveTo(pendingTask)
+                End With
+            End If
+
+            Return
+        End If
+re0:
         Call Moebooru.DownloadPool(pool_id, EXPORT).ToArray
         Call Moebooru.CheckPoolIntegrity(EXPORT) _
                      .GetJson _
                      .PrintException
+
+        If pendingTask.FileExists Then
+            pool_id = pendingTask.LoadObject(Of Dictionary(Of String, String)) _
+                                 .Where(Function(task) task.Value = "0") _
+                                 .FirstOrDefault _
+                                 .Key
+
+            If Not pool_id.StringEmpty Then
+                GoTo re0
+            End If
+        End If
     End Sub
 End Module
