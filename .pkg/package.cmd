868aed9b27a9d6ea69b64b8f4e6c9e1980b5e493
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-11 19:49:38 +0800

    configs for package msbuild

---
+++ .pkg/package.cmd
@@ -0,0 +1,11 @@
+@echo off
+
+SET drive=%~d0
+SET R_HOME=%drive%\GCModeller\src\R-sharp\App\net8.0
+SET Rscript="%R_HOME%/Rscript.exe"
+SET REnv="%R_HOME%/R#.exe"
+
+%Rscript% --build /src ../ /save ../Finance.zip --skip-src-build
+%REnv% --install.packages ../Finance.zip
+
+pause
